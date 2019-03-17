f90debdae6ad7449646557e84cf081106ba3c760
wangjiawei <wjwcoder@gmail.com>
2019-03-17 19:59:18 +0800

    feat: some commit

--- core/lazyClass.js
+++ core/lazyClass.js
@@ -1,4 +1,18 @@
 import Emitter from '../utils/emitter';
-class LazyClass extends Emitter {}
+class LazyClass extends Emitter {
+  constructor(vue, options) {
+    super();
+    this.listenerQueue = [];
+    this.scrollerElementQueue = [];
+    this.vue = vue;
+    this.options = {};
+    this._initEvents();
+  }
+
+  /**
+   *
+   */
+  _initEvents() {}
+}
 
 export default LazyClass;
--- test/utils/emitter.test.js
+++ test/utils/emitter.test.js
@@ -1,10 +1,40 @@
 import Emitter from '../../utils/emitter';
 
-jest.mock('../../utils/emitter');
-
-descripe('Test emitter for eventBus function', () => {
-  it('We can check if the consumer called the class constructor', () => {
+describe('Test emitter for eventBus function', () => {
+  it('check $on and $off', () => {
     const eventBus = new Emitter();
-    expect(Emitter).toHaveBeenCalledTimes(1);
+    const sum = function(a, b) {
+      return a + b;
+    };
+    eventBus.$on('sum', sum);
+    expect(eventBus.$has('sum', sum)).toBeTruthy();
+    eventBus.$off('sum', sum);
+    expect(eventBus.$has('sum', sum)).not.toBeTruthy();
+  });
+
+  it('check $on with $once', () => {
+    const eventBus = new Emitter();
+    let a, b, c;
+    const afn = function(v) {
+      a = v;
+    };
+    const bfn = function(v) {
+      b = v;
+    };
+    const cfn = function(v) {
+      c = v;
+    };
+    eventBus.$on('assign', afn);
+    eventBus.$once('assign', bfn);
+    eventBus.$on('assign', cfn);
+    expect(eventBus._events['assign'].length).toBe(3);
+    eventBus.$emit('assign', 10);
+
+    expect(a).toBe(10);
+    expect(b).toBe(10);
+    expect(c).toBe(10);
+
+    expect(eventBus._events['assign'].length).toBe(2);
+    expect(eventBus.$has('assign', bfn)).toBeFalsy();
   });
 });
