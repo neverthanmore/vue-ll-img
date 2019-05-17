2ba3f00c5a000ebf049bc7f850bdec843da06f4e
wangjiawei <wangjiawei01@corp.netease.com>
2019-05-17 16:48:47 +0800

    fix: isvue2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import LazyClass from './core/lazyClass';
 
 export default {
   install(Vue, options = {}) {
-    const isVue2 = Vue.version.slice(0, 1) === 2;
+    const isVue2 = Vue.version.slice(0, 1) == '2';
 
     if (!isVue2 && !Vue.config.silent) {
       console.error('【vue-lazyLoad】The plugin only support Vue2.x, please change your vuejs version!');
